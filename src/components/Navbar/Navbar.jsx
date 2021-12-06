d781b998b016bdbc649aa88ad8a3808c9efce333
plarrat <larrat.philippe@lessonsharing.fr>
2021-12-06 16:46:55 +0100

    Router et NavBar

--- src/components/Navbar/Navbar.jsx
+++
@@ -1,9 +0,0 @@
-export default function Navbar() {
-  return (
-    <nav class="navbar navbar-light bg-light">
-      <div class="container-fluid">
-        <span class="navbar-brand mb-0 h1">Appli SPA</span>
-      </div>
-    </nav>
-  );
-}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,37 +1,33 @@
 import "./Home.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Navbar from "../../components/Navbar/Navbar";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Navbar />
-      </header>
-
-      <main>
-        <section className="container">
-          <section className="row">
-            <div className="col-md-12">
-              <div className="p-5 m-5 mb-4 bg-light rounded-3">
-                <div className="container-fluid py-5">
-                  <h1 className="display-5 fw-bold">
-                    Gestion des utilisateurs SPA
-                  </h1>
-                  <p className="col-md-8 fs-4">
-                    Le but de l'exercice sera de créer une application single
-                    page, nous permettant de naviguer entre plusieurs pages sans
-                    recharger le navigateur.
-                  </p>
-                  <button className="btn btn-primary btn-lg" type="button">
-                    Gestion des utilisateurs
-                  </button>
-                </div>
+    <main>
+      <section className="container">
+        <section className="row">
+          <div className="col-md-12">
+            <div className="p-5 m-5 mb-4 bg-light rounded-3">
+              <div className="container-fluid py-5">
+                <h1 className="display-5 fw-bold">Gestion des utilisateurs</h1>
+                <p className="col-md-8 fs-4">
+                  Le but de l'exercice sera de créer une application single
+                  page, nous permettant de naviguer entre plusieurs pages sans
+                  recharger le navigateur.
+                </p>
+                <Link
+                  to="/users"
+                  className="btn btn-primary btn-lg"
+                  type="button"
+                >
+                  Gestion des utilisateurs
+                </Link>
               </div>
             </div>
-          </section>
+          </div>
         </section>
-      </main>
-    </div>
+      </section>
+    </main>
   );
 }
